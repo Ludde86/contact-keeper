6a93ce42ac03b437df62c222ec9d8f86e5bf5c08
Ludde86 <bslbjorn@gmail.com>
2020-03-16 13:13:21 +0100

    add function connectDB

--- config/db.js
+++ config/db.js
@@ -4,5 +4,13 @@ const mongoose = require('mongoose');
 // bring in config -> we need access to that global variable
 const config = require('config');
 
-// initialize a variable and grap the mango uri
+// initialize a variable and grab the mango uri (global variable)
 const db = config.get('mongoURI');
+
+// mangoos returns promises
+const connectDB = () => {
+	mongoose.connect().then(() => console.log('MangoDB connected')).catch((err) => {
+		console.log(err.message);
+		process.exit(1); // 1 -> exit with failure
+	});
+};
