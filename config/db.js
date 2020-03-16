edddbf34614d85cd4c3a654255f379ab79aba684
Ludde86 <bslbjorn@gmail.com>
2020-03-16 13:22:11 +0100

    fix pass the mango url

--- config/db.js
+++ config/db.js
@@ -9,10 +9,13 @@ const db = config.get('mongoURI');
 
 // mangoos returns promises
 const connectDB = () => {
-	mongoose.connect().then(() => console.log('MangoDB connected')).catch((err) => {
-		console.log(err.message);
-		process.exit(1); // 1 -> exit with failure
-	});
+	mongoose
+		.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(() => console.log('MangoDB connected'))
+		.catch((err) => {
+			console.log(err.message);
+			process.exit(1); // 1 -> exit with failure
+		});
 };
 
 module.exports = connectDB;
