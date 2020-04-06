8c588c04878648d94d8feb54ad8508f995cf3ccb
Ludde86 <bslbjorn@gmail.com>
2020-04-06 10:39:59 +0200

    add validiation for name, email and password

--- routes/users.js
+++ routes/users.js
@@ -4,21 +4,32 @@ const express = require('express');
 // store the router
 const router = express.Router();
 
+// bring in express validator -> so we can use the chaeck function for validation
+const { check, validationResult } = require('express-validator');
+
 // bring in user model, so we can use it within our routes
 const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-	// '/' goes to '/api/users' -> this file get forwarded to server.js app.use('/api/users', require('./routes/users'))
-	// res.send('Register a user');
-	res.send(req.body);
-	// req.body = the json data sent to the route (the email, password and the name -> {"name": "Ludvig", "email": "ludvig@mail.com"})
-	// -> in order to use request body, we need to add middleware to our server.js
+router.post(
+	'/',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+	],
+	(req, res) => {
+		// '/' goes to '/api/users' -> this file get forwarded to server.js app.use('/api/users', require('./routes/users'))
+		// res.send('Register a user');
+		res.send(req.body);
+		// req.body = the json data sent to the route (the email, password and the name -> {"name": "Ludvig", "email": "ludvig@mail.com"})
+		// -> in order to use request body, we need to add middleware to our server.js
 
-	// error checking
-});
+		// error checking
+	}
+);
 
 // export the route
 module.exports = router;
