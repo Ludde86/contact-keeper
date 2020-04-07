9d02b1a0de06e81ea1cfab941d8f3a374a1d7722
Ludde86 <bslbjorn@gmail.com>
2020-04-07 11:28:33 +0200

    add model contact

---
+++ models/Contact.js
@@ -0,0 +1,36 @@
+// bring in mongoose
+const mongoose = required('mongoose');
+
+// create object schema for contact with mongoose schema method
+const ContactSchema = mongoose.Schema({
+	// create relationship between contacts and users -> each user has it's own set of contacts
+	// -> user gonna be a part of this schema
+	// -> type: an entity with mongo (the document) db has an obect id -> use this type
+	// -> refer to a specific collection -> users
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'users'
+	},
+
+	// add fields for this schema
+	// -> user, name, email, phone, type, date
+	// -> type: string, default 'personal'
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	phone: {
+		type: String
+	},
+	type: {
+		type: String,
+		default: 'personal'
+	}
+});
+
+// export module -> this mongoose model -> model name and model schema
+module.exports = mongoose.model('contact', ContactSchema);
