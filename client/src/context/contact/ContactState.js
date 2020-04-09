07ef767d5f5d2dccfdc639c238ab9506b53bf329
Ludde86 <bslbjorn@gmail.com>
2020-04-09 13:37:57 +0200

    add variables in types.js

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,5 @@
+// import useReducer so we get access to state and dispatch -> so we can dispatch to our reducer
+// import uuid to generate a random id (for hardcoded data before we deal with our backend)
+// import our contact context and contact reducer
+
+//
