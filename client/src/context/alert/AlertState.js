f9b4569a840f06a6ee0bafda2684cc2c9acbb6b5
Ludde86 <bslbjorn@gmail.com>
2020-04-16 10:00:30 +0200

    add AlertState.js

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,9 +1,41 @@
+import { useReducer } from 'react';
 import AlertContext from './alertContext';
+import alertReducer from './alertReducer';
+import { v4 as uuid } from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AlertState = (props) => {
-	const initialState = {};
+	// use array as the state
+	// -> object have a message and a type
+	const initialState = [];
 
-	return <AlertContext.Provider value={{}}>{props.children}</AlertContext.Provider>;
+	const [ state, dispatch ] = useReducer(initialState, alertReducer);
+
+	// set alert
+	// -> generate a random id
+	// -> dispatch -> payload will have a msg, type and id
+	// the alert will remove in a timeout
+	const setAlert = (msg, type, timeout = 5000) => {
+		const id = v4();
+		dispatch({
+			type: SET_ALERT,
+			payload: { msg, type, id }
+		});
+		setTimeout(() => {
+			dispatch({ type: REMOVE_ALERT, payload: id });
+		}, timeout);
+	};
+
+	return (
+		<AlertContext.Provider
+			value={{
+				alerts: state,
+				setAlert
+			}}
+		>
+			{props.children}
+		</AlertContext.Provider>
+	);
 };
 
 export default AlertState;
