cc76f58261097d117a83b2aa5803512837a490b3
Ludde86 <bslbjorn@gmail.com>
2020-04-17 12:04:47 +0200

    fix logout

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -22,7 +22,7 @@ export default (state, action) => {
 				...state,
 				token: null,
 				isAuthenticated: false,
-				loading: true,
+				loading: false,
 				user: null,
 				error: action.payload // put the error message in state
 			};
